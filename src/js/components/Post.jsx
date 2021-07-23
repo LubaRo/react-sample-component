67b8190e4db93ab74251d6455a21fd9e72f6ba38
Lyubov Parfilova <cnlmode@gmail.com>
2021-07-23 20:13:29 +0400

    fix "unique key for lists" warning

--- src/js/components/Post.jsx
+++ src/js/components/Post.jsx
@@ -5,14 +5,14 @@ class Post extends Component {
   render() {
     return React.createElement(
       "div", { className: 'post-content' }, React.createElement(
-        "div", { className: 'post card border-success mb-3' }, [
+        "div",
+        { className: 'post card border-success mb-3' },
           React.createElement(
             "div", { className: 'post-author card-header bg-success text-white', id: this.props.id }, this.props.user
           ),
           React.createElement(
             "div", { className: 'post-body card-body' }, this.props.content
           ),
-        ]
       ),
       this.props.children
     )
